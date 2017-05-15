75681e37fcffc9371f29efff591a705e0964072f
Chris Dempewolf <cdempewolf@simplymeasured.com>
2017-05-15 11:24:56 -0700

    Update schema
    The reason for using i+1 is documented here:
    https://github.com/simplymeasured/kong/commit/031542f801ee13e12825f5357691d42dc2fe42c5

--- kong/plugins/sm-rate-limiting/schema.lua
+++ kong/plugins/sm-rate-limiting/schema.lua
@@ -9,8 +9,9 @@ return {
     day = { type = "number" },
     month = { type = "number" },
     year = { type = "number" },
-    async = { type = "boolean", default = false },
-    continue_on_error = { type = "boolean", default = false }
+    limit_by = { type = "string", enum = {"consumer", "credential", "ip"}, default = "consumer" },
+    policy = { type = "string", enum = {"local", "cluster"}, default = "cluster" },
+    fault_tolerant = { type = "boolean", default = true }
   },
   self_check = function(schema, plugin_t, dao, is_update)
     local ordered_periods = { "second", "minute", "hour", "day", "month", "year"}
@@ -24,7 +25,7 @@ return {
         if plugin_t[v] <=0 then
           invalid_value = "Value for "..v.." must be greater than zero"
         else
-          for t = i, #ordered_periods do
+          for t = i+1, #ordered_periods do
             if plugin_t[ordered_periods[t]] and plugin_t[ordered_periods[t]] < plugin_t[v] then
               invalid_order = "The limit for "..ordered_periods[t].." cannot be lower than the limit for "..v
             end
